1ab6d784e985a4d8497c1543b92012191460f3f6
seoseuo <seoseuo@naver.com>
2024-09-17 17:15:30 +0900

    DB conn Test 성공

--- SSK_Web/src/main/java/com/ssk/biz/student/StudentDAO.java
+++ SSK_Web/src/main/java/com/ssk/biz/student/StudentDAO.java
@@ -1,7 +1,40 @@
 package com.ssk.biz.student;
 
-public class StudentDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-	
-	
+import com.ssk.biz.common.JDBCUtil;
+
+public class StudentDAO {
+	// JDBC 관련 변수
+	private Connection conn;
+	private PreparedStatement stmt;
+	private ResultSet rs;
+
+	private static final String BOARD_LIST = "select * from board order by seq desc";
+
+	public void test() {
+		try {
+
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_LIST);
+
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+
+				System.out.println(rs.getString("TITLE"));
+
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+	}
+
 }
